5c38ec248e8db808f595fe1b75ff040b2a9af7b4
burkiashvili <burkiashvilidato@gmail.com>
2025-01-22 18:33:23 +0400

    Pirveli

---
+++ app.py
@@ -0,0 +1,39 @@
+# a = input("ramdeni wlis xar?")
+# b = 2025
+# print (a)
+# print(b-int(a))
+
+
+# a=15
+# # b=4.7
+# # print(int(a+b))
+
+# a=input("tqveni saxeli")
+# b="gamarjoba"
+# print(b,a)
+
+
+# a=input("tqveni gvari")
+# print(a.upper())
+
+# a="საქართველო"
+# print(a[2:-1])
+
+
+# a=input("tqveni saxeli?")
+# print(a[-1])
+
+
+a=input
+b=input
+
+
+
+# a=15
+# b=10
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a!=b)
+
+
